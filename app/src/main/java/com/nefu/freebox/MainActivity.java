9361d4e58f6926e0e28ced9dabcba64257794e8d
Hnx <2206299036@qq.com>
2018-01-20 13:40:32 +0800

    master

--- app/src/main/java/com/nefu/freebox/MainActivity.java
+++ app/src/main/java/com/nefu/freebox/MainActivity.java
@@ -1,5 +1,10 @@
 package com.nefu.freebox;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,12 +14,60 @@ import android.widget.Toast;;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Home");
+            //actionBar.setHomeAsUpIndicator(R.mipmap.touxiang);
+        }
+        navigationView.setCheckedItem(R.id.nav_home);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        toolbar.setTitle("Home");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_msg:
+                        toolbar.setTitle("Message");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_myhouse:
+                        toolbar.setTitle("My House");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_stars:
+                        toolbar.setTitle("Stars");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_history:
+                        toolbar.setTitle("History");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_setting:
+                        toolbar.setTitle("Setting");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    case R.id.nav_abouts:
+                        toolbar.setTitle("Abouts");
+                        mDrawerLayout.closeDrawers();
+                        break;
+                    default:
+                }
+                return true;
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
@@ -27,6 +80,9 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.menu_search:
                 Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
         }
